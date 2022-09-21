f8a026afc642a4798d149e9ce055aa07abfd64cf
gekoke <gekoke@lazycantina.xyz>
2022-09-21 18:04:47 +0300

    Make config.el help comment shorter

--- config.el
+++ config.el
@@ -3,18 +3,13 @@
 ;; Whenever you reconfigure a package, make sure to wrap your config in an
 ;; `after!' block, otherwise Doom's defaults may override your settings. E.g.
 ;;
-;;   (after! PACKAGE
-;;     (setq x y))
-;;
 ;; The exceptions to this rule:
-;;
 ;;   - Setting file/directory variables (like `org-directory')
 ;;   - Setting variables which explicitly tell you to set them before their
 ;;     package is loaded (see 'C-h v VARIABLE' to look up their documentation).
 ;;   - Setting doom variables (which start with 'doom-' or '+').
 ;;
-;; Here are some additional functions/macros that will help you configure Doom.
-;;
+;; Additional functions/macros that will help you configure Doom:
 ;; - `load!' for loading external *.el files relative to this one
 ;; - `use-package!' for configuring packages
 ;; - `after!' for running code after a package has loaded
