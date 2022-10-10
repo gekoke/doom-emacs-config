efd39abd40b6f780bcfa4b9c49d1e6d458302a1b
gekoke <gekoke@lazycantina.xyz>
2022-10-10 18:34:16 +0300

    Add Prolog LSP config

--- config.el
+++ config.el
@@ -143,7 +143,20 @@
           ("NO"   . +org-todo-cancel))))
 
 ;; Prolog
-(add-to-list 'auto-mode-alist '("\\.pl\\'" . prolog-mode))
+(after! lsp-mode
+  (add-to-list 'lsp-language-id-configuration '(prolog-mode . "prolog-ls"))
+  (lsp-register-client
+   (make-lsp-client
+    :new-connection
+    (lsp-stdio-connection (list "swipl"
+                                "-g" "use_module(library(lsp_server))."
+                                "-g" "lsp_server:main"
+                                "-t" "halt"
+                                "--" "stdio"))
+    :major-modes '(prolog-mode)
+    :priority 1
+    :multi-root t
+    :server-id 'prolog-ls)))
 
 ;; Latex
 (setq org-latex-compiler "lualatex")
